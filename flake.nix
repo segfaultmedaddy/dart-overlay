e392916e02d83e3ef2c29415ca3c3c980013e134
Roman Vanesyan <roman@vanesyan.com>
2024-10-19 02:51:12 +0300

    feat: add GH actions

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Dart binaries. Supporting stable, beta and dev channels.";
+  description = "Dart binaries. Supporting stable, beta, and dev channels.";
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
